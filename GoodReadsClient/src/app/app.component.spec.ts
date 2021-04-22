85768d91f6b58e75b3495a45a4669dc047f3a20f
chrisQua3od <christengeorge25@gmail.com>
2021-04-23 00:19:01 +0200

    organizing the branch

--- GoodReadsClient/src/app/app.component.spec.ts
+++ GoodReadsClient/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'GoodReadsClient'`, () => {
+  it(`should have as title 'goodReadsAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('GoodReadsClient');
+    expect(app.title).toEqual('goodReadsAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('GoodReadsClient app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('goodReadsAngular app is running!');
   });
 });
--- GoodReadsClient/src/app/app.module.ts
+++ GoodReadsClient/src/app/app.module.ts
@@ -1,16 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from './services/auth.service';
+import { InterceptorService } from './services/interceptor.service';
+import { CategoryComponent } from './components/category/category.component';
+import { AuthorService } from './services/authors.service';
+import { UsersService } from './services/users.service';
+import { HomeService } from './services/home.service';
+import { RatingModule } from 'ng-starrating';
+import { CommonModule } from '@angular/common';  
+import { CategoryDetailsComponent } from './components/category-details/category-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    CategoryDetailsComponent,
+    CategoryComponent,
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    ReactiveFormsModule,
+    NgbModule,
+    RatingModule,
+
+    // RouterModule.forRoot(routes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    AuthService,
+    AuthorService,
+    UsersService,
+    HomeService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
